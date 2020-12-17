15e0b7cc7f6ee7ddb9b6a1d07185354e29056ad1
x18131212 <x18131212@student.ncirl>
2020-12-17 20:35:08 +0000

    Revert "Revert "Complete working code""
    This reverts commit 3b8c64a9c640ea0d014901287d708d5df64eef7c.

--- MovieApp/src/main/java/com/mycompany/movieapp/database/Database.java
+++ MovieApp/src/main/java/com/mycompany/movieapp/database/Database.java
@@ -31,15 +31,27 @@ public class Database {
            //First Movie 
            Movie movie1 = new Movie(movieTB.size()+1,"Red Wedding", "Watch", "Ting's Documentary");
            movieTB.add(movie1);
+           Movie movie4 = new Movie(movieTB.size()+1,"Yellow Wedding", "Watch", "Marcis's Documentary");
+           movieTB.add(movie4);
            //Second Movie
+<<<<<<< Updated upstream
            Movie movie2 = new Movie(movieTB.size()+1,"Blue Wedding", "Recommended", "TJ's Documentary");
+=======
+           Movie movie2 = new Movie(movieTB2.size()+1,"Blue Wedding", "Recommended", "TJ's Documentary");
+>>>>>>> Stashed changes
            movieTB2.add(movie2);
+           Movie movie3 = new Movie(movieTB2.size()+1,"green Wedding", "Recommended", "Jegans's Documentary");
+           movieTB2.add(movie3);
             //First Accounts for customer 
            Account account1 = new Account(accountsTB.size()+1, "Adult", "Mum's account", 123456, movieTB);
            accountsTB.add(account1);
+            Account account3 = new Account(accountsTB.size()+1, "Child", "Step son", 12361231, movieTB2);
+           accountsTB.add(account3);
             //Second Accounts for customer 
-           Account account2 = new Account(accountsTB.size()+1, "Child", "Jegan's account", 111111, movieTB2);
+           Account account2 = new Account(accountsTB2.size()+1, "Child", "Jegan's account", 111111, movieTB2);
            accountsTB2.add(account2);            
+           Account account4 = new Account(accountsTB2.size()+1, "Child", "Dad's account", 112345, movieTB);
+           accountsTB2.add(account4);            
             //First Customer
             Customer customer1 = new Customer("Abraham", "Dublin 2", "Marcus@gmail.com", customerTB.size()+1,accountsTB);
             customerTB.add(customer1);
--- MovieApp/src/main/java/com/mycompany/movieapp/resources/MovieResource.java
+++ MovieApp/src/main/java/com/mycompany/movieapp/resources/MovieResource.java
@@ -37,7 +37,13 @@ public class MovieResource {
     @GET//geta specific movie
     @Path("/{movieId}")
     public Movie getMovie(@PathParam("customerId") int customerId, @PathParam("accountId") int accountId, @PathParam("movieId") int movieId) {
+<<<<<<< Updated upstream
  
+=======
+        accountId = accountId-1;
+        customerId = customerId-1;
+        movieId=movieId-1;
+>>>>>>> Stashed changes
         return MovieService.getMovie(customerId, accountId, movieId);
     }
     
@@ -47,6 +53,7 @@ public class MovieResource {
       return  MovieService.addMovie(movie, accountId, customerId);
     }
 
+<<<<<<< Updated upstream
      @DELETE
     @Path("/{MovieID}")
     public String removeMovie(@PathParam("customerId") int CustomerID, @PathParam("accountId") int AccountID, @PathParam("MovieID") int movieid) {
@@ -55,18 +62,54 @@ public class MovieResource {
         MovieService.removeMovie(movie, AccountID, CustomerID);
 
         return "The"  + deletedMovie.getMovieName()+ "Was Removed from: User with Customer Id"+CustomerID+1;
+=======
+    @DELETE
+    @Path("/{movieId}")
+    public String removeMovie(@PathParam("customerId") int customerId,@PathParam("accountId") int accountId, @PathParam("movieId") int mId){
+        int mid1,accountId1,customerId1;
+        mid1 = mId-1;
+        accountId1 =accountId-1;
+        customerId1 = customerId - 1; 
+        Movie movie=MovieService.getMovie(customerId1, accountId1, mid1);
+        Movie movie2=movie;
+        MovieService.removeMovie(movie, accountId1, customerId1);
+        return "The movie "+movie2.getMovieName()+" was removed";
+>>>>>>> Stashed changes
     }
     
     @POST
      @Path("/{movieId}/{transAccId}")
      public String transferMovie(@PathParam("customerId")int customerId ,@PathParam("accountId")int custAccId ,@PathParam("transAccId")int transAccId ,@PathParam("movieId") int movieId){
-     Movie movie= MovieService.getMovie(customerId, custAccId, movieId);//getting the movie with its id
+     int customerId1,custAccId1,transAccId1,movieId1;
+     customerId1=customerId-1;
+     custAccId1=custAccId-1;
+     transAccId1=transAccId-1;
+     movieId1=movieId-1;
+     
+         
+     Movie movie= MovieService.getMovie(customerId1, custAccId1, movieId1);//getting the movie with its id
      Movie  movie2Transfer=movie;
-     MovieService.removeMovie(movie, custAccId, customerId);//removing teh move from teh current holder of that movie
-     MovieService.addMovie(movie2Transfer,transAccId, custAccId);
+     MovieService.removeMovie(movie, custAccId1, customerId1);//removing teh move from teh current holder of that movie
+     MovieService.addMovie(movie2Transfer,transAccId1, custAccId1);
+     AccountResource acc=new AccountResource();
     
   
+<<<<<<< Updated upstream
      return "the movie transferred was: " +movie2Transfer+"\n to: "+transAccId;
+=======
+     return "The movie " +movie2Transfer.getMovieName()+"was transferred to Account nikename "+acc.getAccountNickname(custAccId1, transAccId1)
+             +"with Acc no"+(transAccId+1);
+     
+  
+     } 
+ 
+    
+    
+    
+    
+    
+    
+>>>>>>> Stashed changes
     
 }
 }
